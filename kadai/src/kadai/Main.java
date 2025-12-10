8b3382eaeda57e360642ba12c18f13f85a7907c5
mozuk <mozuk@LAPTOP-L8EE5TJB>
2025-12-10 14:01:46 +0900

    【12-2 課題1】を修正しました（周囲の長さは、内角の和は、という文字列を追加）

--- kadai/src/kadai/Main.java
+++ kadai/src/kadai/Main.java
@@ -7,27 +7,27 @@ public class Main {
 		if (number == 0) {
 			Circle c = new Circle(100, 100, 20);
 			c.draw();
-			System.out.println(c.getPerimeter());
+			System.out.println("周囲の長さは、" + c.getPerimeter());
 
 		} else if (number == 2) {
 			Line l = new Line(0, 0, 100, 100);
 			l.draw();
-			System.out.println(l.getPerimeter());
+			System.out.println("周囲の長さは、" + l.getPerimeter());
 		} else if (number == 3) {
 			Triangle t = new Triangle(0, 0, 100, 100, 0, 200);
 			t.draw();
-			System.out.println(t.getPerimeter());
-			System.out.println(t.getInternalAngle());
+			System.out.println("周囲の長さは、" + t.getPerimeter());
+			System.out.println("内角の和は、" + t.getInternalAngle());
 		} else if (number == 4) {
 			Rectangle e = new Rectangle(0, 0, 100, 50);
 			e.draw();
-			System.out.println(e.getPerimeter());
-			System.out.println(e.getInternalAngle());
+			System.out.println("周囲の長さは、" + e.getPerimeter());
+			System.out.println("内角の和は、" + e.getInternalAngle());
 		} else if (number == 44) {
 			Square s = new Square(0, 0, 200);
 			s.draw();
-			System.out.println(s.getPerimeter());
-			System.out.println(s.getInternalAngle());
+			System.out.println("周囲の長さは、" + s.getPerimeter());
+			System.out.println("内角の和は、" + s.getInternalAngle());
 		}
 	}
 }
