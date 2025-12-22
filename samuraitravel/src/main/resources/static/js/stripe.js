8646108896761dda16e6ec4099a1ff2db730b1be
mozuk <mozuk@LAPTOP-L8EE5TJB>
2025-12-22 12:40:03 +0900

    ホテルル1周目

---
+++ samuraitravel/src/main/resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+const stripe = Stripe('pk_test_51SgFvpAZ8ywDGuum5ZRgxKJXYvxAmSiIO6c3I2mshksS2xRSUExcrLjQCxIb60Fdd6IWzROlpApDalTAKiwt6RcW00N2UCMKjV');
+const paymentButton = document.querySelector('#paymentButton');
+
+paymentButton.addEventListener('click',()=>{
+	stripe.redirectToCheckout({
+		sessionId:sessionId
+	})
+});
